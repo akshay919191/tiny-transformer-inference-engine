3d4ddfc673bae70735b89ab99a169822f4f7e5cb
akshay919191 <sharmaakshay86117@gmail.com>
2026-09-08 14:47:04 +0530

    config fxD

--- models/model_config.py
+++ models/model_config.py
@@ -1,18 +1,17 @@
 from dataclasses import dataclass
 import torch
 
-## initially tiny
 @dataclass
 class ModelConfig:
     vocab_size: int = 50257
     hidden_size: int = 128
     num_layers: int = 8
     num_heads: int = 8
-    num_kv_heads : int = 1
+    num_kv_heads: int = 1
     max_seq_len: int = 512
-    d_model : int  = 256
-    casual = True
-    dropout = 0.0
-    bias = False
-    batch = 4
-    dtype = torch.float32
+    d_model: int = 256
+    causal: bool = True       
+    dropout: float = 0.0     
+    bias: bool = False        
+    batch: int = 4            
+    dtype: torch.dtype = torch.float32  
